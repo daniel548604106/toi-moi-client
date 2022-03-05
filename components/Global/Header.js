ca1654294fa54569509475dfd0f38aeec7211a33
Daniel Yeh <daniel548604106@gmail.com>
2022-03-05 12:15:26 +0800

    fix animate tabbar

--- components/Global/Header.js
+++ components/Global/Header.js
@@ -8,6 +8,7 @@ import {
   UserGroupIcon,
   PlusIcon,
 } from '@heroicons/react/solid';
+
 import useClickOutside from '@/Hooks/useClickOutside';
 import HeaderIcon from './HeaderIcon';
 import genderAvatar from '@/Utils/genderAvatar';
@@ -53,15 +54,14 @@ const Header = () => {
   const router = useRouter();
   const elRef = useRef();
   const dispatch = useDispatch();
-  const [isSideMenuShow, setSideMenuShow] = useState(false);
 
-  useEffect(() => {
-    console.log(router.pathname);
-  }, [router.pathname]);
+  const { t } = useTranslation('header');
   useClickOutside(elRef, () => setSideMenuShow(false));
   const { userInfo } = useSelector((state) => state.user);
 
-  const { t } = useTranslation('header');
+  const [isSideMenuShow, setSideMenuShow] = useState(false);
+  const [activeTab, setActiveTab] = useState(router.pathname);
+
   const handleSideMenuShow = (e) => {
     e.stopPropagation();
     setSideMenuShow(!isSideMenuShow);
@@ -84,8 +84,14 @@ const Header = () => {
           router.pathname.includes('messages') && 'hidden'
         } fixed max-w-[750px] flex items-center top-[50px] bg-secondary text-secondary left-0    w-full md:static  flex-grow sm:px-5 sm:mx-0 xl:px-10`}
       >
-        {menuTabs.map((tab, idx) => (
-          <HeaderIcon key={tab.title} {...tab} />
+        {menuTabs.map(({ title, href, Icon }) => (
+          <HeaderIcon
+            key={title}
+            href={href}
+            Icon={Icon}
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
         ))}
       </div>
       <div className="w-1/2  relative flex justify-end items-center space-x-1 sm:space-x-2 ">
